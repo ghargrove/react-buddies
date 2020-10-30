3f34a09117b8090d30ea257cb6fe5c98b3c726ca
Graham Hargrove <hargrove.graham@yahoo.com>
2020-10-30 17:21:05 -0400

    Added styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './App.css';
+
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Buddies meeting</h1>
+      <h1 className="headline">Buddies meeting</h1>
     </div>
   )
 };
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,15 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /tsx?$/, use: 'ts-loader' },
+      {
+        test: /tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
   },
   plugins: [
